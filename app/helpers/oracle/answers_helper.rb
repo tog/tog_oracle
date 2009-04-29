fe4689d967c112c68e76b2486d04ba2489456592
Balint Erdi <balint.erdi@gmail.com>
2009-04-29 14:11:29 +0200

    answers are rateable
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- app/helpers/oracle/answers_helper.rb
+++ app/helpers/oracle/answers_helper.rb
@@ -21,4 +21,26 @@ module AnswersHelper
   def answer_this_question_link(question)
     link_to I18n.t("oracle.answers.answer_this_question"), new_member_oracle_question_answer_path(question) if @question.opened?
   end
+  
+  def answer_and_rating_stars(answer)
+    out = []
+    out << answer.body
+    out << rating_stars(answer,'ico/star_on.gif','ico/star.gif')
+    out << content_tag(:span, "SUITABLE") if answer.suitable?
+    out.join(" ")
+  end
+  
+  # TODO: this should go to tog_core into the RatesHelper module
+  # Till then it stays here, since we cannot rely on the host app defining this helper.
+  def rating_stars(rateable, filled, empty)
+    if rateable.total_ratings > 0
+      return stars(rateable, :filled_star => image_tag(filled), :empty_star => image_tag(empty)) do |content,rating,filled|
+        link_to content, member_rate_path(:type => rateable.class.name, :id => rateable.id, :rate => rating), :id => "rating-link-#{rating}"
+      end
+    else
+      return [1,2,3,4,5].collect{|i| 
+        link_to image_tag(empty), member_rate_path(:type => rateable.class.name, :id => rateable.id, :rate => i), :id => "rating-link-#{i}"
+      }
+    end
+  end
 end
--- app/models/oracle/answer.rb
+++ app/models/oracle/answer.rb
@@ -1,10 +1,12 @@
 class Oracle::Answer < ActiveRecord::Base
-  
+
   belongs_to :question, :class_name => "Oracle::Question"
   belongs_to :user
 
   validates_presence_of :body
-  
+
+  acts_as_rateable :average => true
+
   def make_suitable!
     self.suitable = true
   end
@@ -12,7 +14,7 @@ class Oracle::Answer < ActiveRecord::Base
   def make_unsuitable!
     self.suitable = false
   end
-  
+
   def suitable?
     !!self.suitable
   end
