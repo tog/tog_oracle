38cb52777ed3f89b779499dbb6e2ce37f73a5bd9
Andrei Erdoss <erdoss@gmail.com>
2009-05-12 12:55:19 +0800

    Changed test helper
    Signed-off-by: tog - extensible open source social network platform <tog@linkingpaths.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,18 @@
 ENV["RAILS_ENV"] = "test"
-require 'rubygems'
+#require 'rubygems'
 #FIXME: I only inserted this so I can run tests without clashing with rails 2.3
 # once this issue is fixed, this should be removed
-gem "activesupport", "=2.2.2"
-require 'active_support'
-require 'active_support/test_case'
+#gem "activesupport", "=2.2.2"
+#require 'active_support'
+#require 'active_support/test_case'
 
 #IMPORTANT! This line should not be moved under "require 'factory_girl'"
 # factory_girl tries to load the test/factories file (of the host app, not the plugin!)
 # before the models are defined and will crash miserably.
 require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
+
+require 'test_help'
+require 'test/unit'
 
 require 'shoulda'
 require "shoulda/action_controller"
@@ -17,7 +20,7 @@ require "shoulda/action_controller"
 require 'factory_girl'
 require File.expand_path(File.dirname(__FILE__) + '/factories')
 
-class Test::Unit::TestCase
+class ActiveSupport::TestCase
   def create_user_without_validation(opts={})
     user = User.new(opts)
     user.save(false)
