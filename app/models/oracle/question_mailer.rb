a5afaa62c659f4c5b217b95e0890afae2b15c792
Balint Erdi <balint.erdi@gmail.com>
2009-06-01 11:14:50 +0200

    stripping html tags from overdue question notif. (plain) email Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- app/models/oracle/question_mailer.rb
+++ app/models/oracle/question_mailer.rb
@@ -1,13 +1,14 @@
 class Oracle::QuestionMailer < ActionMailer::Base
-  
+
   def close_question_request(question)
     publisher = question.user
     @recipients  = "#{publisher.email}"
     @from        = Tog::Config["plugins.tog_core.mail.system_from_address"]
     @subject     = 'Please pick a suitable answer'
-    
+
     @body[:asker_name] = publisher.profile.name
-    @body[:question] = question.body
+    # strip tags and html entities from question body
+    @body[:question] = question.body.gsub(/\<[^>]*\>/, '').gsub(/&.*?;/, '')
     @body[:answer_link] = oracle_question_answers_url(question)
     @body[:sender_name] = Tog::Config["plugins.tog_core.mail.system_from_address"]
   end
